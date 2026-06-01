f5868388687dc4fc829f83f061fc0417e8977308
Gustavo de Paula Silva <gustavodepaulasilva299@gmail.com>
2026-06-01 20:29:36 -0300

    Add bcryptjs for password hashing

--- controller/usuario/controller_usuario.js
+++ controller/usuario/controller_usuario.js
@@ -11,6 +11,7 @@ const mesagensDefault = require("../modulo/config_messages.js")
 const validarDados = require("../modulo/validar_dados.js")
 const validarAtributos = require("../modulo/validar_atributos.js")
 const jwt = require('../../jwt/jwt_service.js')
+const bcrypt = require('bcryptjs');
 const { json } = require("express")
 const crypto = require('crypto');
 // GET +
